67bd46f1b60f8f38db889ac588f437e4e2d8fa1f
UgoDIA <ugofokyin@gmail.com>
2023-02-20 10:03:49 +0400

    models

--- src/classifr/models.py
+++ src/classifr/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+class Classe(models.Model):
+    nom_classe = models.CharField(primary_key=True, max_length=30)
+
+    class Meta:
+        
+        db_table = 'classe'
+        
+class Model(models.Model):
+    nom_model = models.CharField(primary_key=True, max_length=20)
+
+    class Meta:
+        
+        db_table = 'model'        
+
+
+class Historique(models.Model):
+    id_histo = models.AutoField(primary_key=True)
+    date_pred = models.DateField()
+    classe_predit = models.ForeignKey(Classe,models.DO_NOTHING,related_name='classe_preditfk', db_column='classe_predit')
+    precision = models.DecimalField(max_digits=5, decimal_places=2)
+    classe_correcte = models.ForeignKey(Classe, models.DO_NOTHING,related_name='classe_correctefk', db_column='classe_correcte', blank=True, null=True)
+    path = models.CharField(max_length=100)
+    model = models.ForeignKey('Model', models.DO_NOTHING, db_column='model')
+
+    class Meta:
+        
+        db_table = 'historique'
