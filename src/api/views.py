629c36e654984bb9bb454943988bb864bd71861f
UgoDIA <ugofokyin@gmail.com>
2023-03-07 09:31:58 +0400

    fix

--- src/api/views.py
+++ src/api/views.py
@@ -55,24 +55,47 @@ def getstats(request):
     # print(totalModel)
     cursor.execute('''select nom_model,count(*)
                     from historique 
-                    where classe_correcte is null 
+                    where classe_correcte is null and nom_model='model3'
                     group by (nom_model)
                     order by nom_model desc''')
-    totalModelSuccess=cursor.fetchall()
+    totalModel3Success=cursor.fetchall()
+    if not totalModel3Success:
+        totalModel3Success=[('0',0)]
+    cursor.execute('''select nom_model,count(*)
+                    from historique 
+                    where classe_correcte is null and nom_model='model11'
+                    group by (nom_model)
+                    order by nom_model desc''')
+    totalModel11Success=cursor.fetchall()
+    if not totalModel11Success:
+        totalModel11Success=[('0',0)]
     # print(totalModelSuccess)
     cursor.execute('''select nom_model,count(*)
                     from historique 
-                    where classe_correcte is not null 
+                    where classe_correcte is not null and nom_model='model3'
                     group by (nom_model)
                     order by nom_model desc''')
-    totalModelError=cursor.fetchall()
-    # print(totalModelError)
+    totalModel3Error=cursor.fetchall()
+    if not totalModel3Error:
+        totalModel3Error=[('0',0)]
+    # print(totalModel3Error)
+    cursor.execute('''select nom_model,count(*)
+                    from historique 
+                    where classe_correcte is not null and nom_model='model11'
+                    group by (nom_model)
+                    order by nom_model desc''')
+    totalModel11Error=cursor.fetchall()
+    if not totalModel11Error:
+        totalModel11Error=[('0',0)]
+    # print(totalModel11Error)
     cursor.execute('''select classe_predit,count(*)
                     from historique 
                     where nom_model='model3' and classe_correcte is null
                     group by (classe_predit)
                     order by 2 desc''')
     topModel3=cursor.fetchall()
+    if not topModel3:
+        topModel3=[('0',0)]
     # print(topModel3)
     cursor.execute('''select classe_correcte,count(*)
                     from historique 
@@ -80,6 +103,8 @@ def getstats(request):
                     group by (classe_correcte)
                     order by 2 desc''')
     flopModel3=cursor.fetchall()
+    if not flopModel3:
+        flopModel3=[('0',0)]
     # print(flopModel3)
     cursor.execute('''select classe_predit,count(*)
                     from historique 
@@ -87,6 +112,8 @@ def getstats(request):
                     group by (classe_predit)
                     order by 2 desc''')
     topModel11=cursor.fetchall()
+    if not topModel11:
+        topModel11=[('0',0)]
     # print(topModel11)
     cursor.execute('''select classe_correcte,count(*)
                     from historique 
@@ -94,22 +121,24 @@ def getstats(request):
                     group by (classe_correcte)
                     order by 2 desc''')
     flopModel11=cursor.fetchall()
+    if not flopModel11:
+        flopModel11=[('0',0)]
     # print(flopModel11)
     result=[{
             "nomModel":totalModel[0][0],
             "total":totalModel[0][1],
-            "success":totalModelSuccess[0][1],
-            "errors":totalModelError[0][1],
-            "pourcentage":round(totalModelSuccess[0][1] / totalModel[0][1] * 100,2),
+            "success":totalModel3Success[0][1],
+            "errors":totalModel3Error[0][1],
+            "pourcentage":round(totalModel3Success[0][1] / totalModel[0][1] * 100,2),
             'top':topModel3[0][0],
             'flop':flopModel3[0][0],     
         },
           {
             "nomModel":totalModel[1][0],
             "total":totalModel[1][1],
-            "success":totalModelSuccess[1][1],
-            "errors":totalModelError[1][1],
-            "pourcentage":round(totalModelSuccess[1][1] / totalModel[1][1] * 100,2),
+            "success":totalModel11Success[0][1],
+            "errors":totalModel11Error[0][1],
+            "pourcentage":round(totalModel11Success[0][1] / totalModel[1][1] * 100,2),
             'top':topModel11[0][0],
             'flop':flopModel11[0][0],            
     }]
